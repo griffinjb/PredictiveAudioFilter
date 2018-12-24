449a8f9020094ef91fb7dfe54bb50956072767a4
griffinjb <griffinj@oregonstate.edu>
2018-12-23 16:17:12 -0800

    crashes on large files

--- tsf.py
+++ tsf.py
@@ -21,11 +21,13 @@ import matplotlib.pyplot as plt
 
 from scipy.io import wavfile
 
+from time import strftime
+
 from keras import Sequential
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
-
+from keras.models import model_from_json
 
 # load audio to nparray, return 0,1 scaled nparray
 def loadWav(path):
@@ -92,7 +94,7 @@ def tspLabelFormat(data,bufferSize,predictionSize):
 	return([sets,labels])
 
 # predictive synthesis - signal [0->1]
-def generatePredictions(model,signal,bufferSize,predictionSize):
+def generatePredictions(model,signal,bufferSize,predictionSize,fullReplacement):
 	# damn so this will be kinda hard
 
 	# Input is clipped audio
@@ -104,62 +106,47 @@ def generatePredictions(model,signal,bufferSize,predictionSize):
 	# for each sample, insert sample from either unclipped audio
 	# or from prediction.
 
-	predBuffer = signal[0:bufferSize]
+	predBuffer = [[signal[0:bufferSize]]]
+	predBuffers = [predBuffer]
 
 	pred = model.predict(predBuffer)
 
+	clipCtr = 0
+
+	print(len(signal))
+
 	for idx,sample in enumerate(signal[bufferSize:-predictionSize]):
 
+		if idx%1000 == 0:
+			print(str(idx)+'/'+str(len(signal))+'~ %'+str(100*idx/len(signal)),end="\r")
+
 		# Append prediction or sample
-		predBuffer[:-1] = predBuffer[1:]
-
+		predBuffer[0][0][:-1] = predBuffer[0][0][1:]
+	
 		# replace corrupted signal with predicted signal
-		if sample == 1 or sample == 0:
+		if sample == 1 or sample == 0 or fullReplacement:
 			signal[bufferSize:-predictionSize][idx] = pred
+			clipCtr += 1
 
-		predBuffer[-1] = sample
+		predBuffer[0][0][-1] = sample
 
 		# Make prediction
 		pred = model.predict(predBuffer)
-		predBuffer
 
+	print("\nClip Count"+str(clipCtr))
 
+	return(signal)
 
-if __name__ == "__main__":
-	
-	# Parameters
-	audioPath = getcwd()+"/liteSpdRKT.wav"
-	bufferSize = 2
-	predictionSize = 1
+def saveModel(model):
+	# serialize model to JSON
+	model_json = model.to_json()
+	with open("model"+strftime("%Y%m%d-%H%M%S")+".json", "w") as json_file:
+	    json_file.write(model_json)
+	# serialize weights to HDF5
+	model.save_weights("model"+strftime("%Y%m%d-%H%M%S")+".h5")
+	print("Saved model to disk")
 
-	# yo = rampGen()
-	wv,rate = loadWav(audioPath)
-
-	# wv = wv[0:1000000]
-
-	parsedClip = clippingParser(wv)
-
-	dataLabel = []
-
-	f = 1
-
-	# iterate clips and append labels
-	for yo in parsedClips:
-		if f:
-			dataLabel = tspLabelFormat(yo,bufferSize,predictionSize)
-			f = 0
-		else:
-			temp = tspLabelFormat(yo,bufferSize,predictionSize)
-			for dat in temp[0]:
-				dataLabel[0].append(dat)
-			for dat in temp[1]:
-				dataLabel[1].append(dat)
-
-	# Zip_dataLabel = zip(dataLabel[0],dataLabel[1])
-
-	X_train = np.asarray(dataLabel[0])
-	y_train = np.asarray(dataLabel[1])
-
+def trainModel(X_train,y_train):
 	# now classify boyo
 	# layers = Sequential()
 	model = Sequential()
@@ -172,18 +159,77 @@ if __name__ == "__main__":
 	sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 	model.compile(loss='mean_squared_error',
 	              optimizer=sgd)
-
 	# train model
 	model.fit(X_train, y_train, epochs=1, batch_size=500)
 
-	# save model to memory
+	return(model)
 
+def loadModel(jsonfn,h5fn):
+	# load json and create model
+	json_file = open(jsonfn, 'r')
+	loaded_model_json = json_file.read()
+	json_file.close()
+	loaded_model = model_from_json(loaded_model_json)
+	# load weights into new model
+	loaded_model.load_weights(h5fn)
+	print("Loaded model from disk")
+	return(loaded_model)
+
+if __name__ == "__main__":
+	
+	# Parameters
+	audioPath = getcwd()+"/liteSpdRKT.wav"
+	bufferSize = 200
+	predictionSize = 1
+	# newModel = False
+	newModel = True
+	fullReplacement = True
+
+	# yo = rampGen()
+	wv,rate = loadWav(audioPath)
+	print("Audio Loaded")
+
+	# wv = wv[1100000:1200000]
+
+	print("Parsing into non-clipped data")
+	parsedClips = clippingParser(wv)
+	print("Parsing Complete")
+
+	dataLabel = []
+
+	f = 1
+
+	print("Formatting labels")
+	# iterate clips and append labels
+	for yo in parsedClips:
+		if f:
+			dataLabel = tspLabelFormat(yo,bufferSize,predictionSize)
+			f = 0
+		else:
+			temp = tspLabelFormat(yo,bufferSize,predictionSize)
+			for dat in temp[0]:
+				dataLabel[0].append(dat)
+			for dat in temp[1]:
+				dataLabel[1].append(dat)
+	print("Formatting Complete")
+
+	# Zip_dataLabel = zip(dataLabel[0],dataLabel[1])
+
+	X_train = np.asarray(dataLabel[0])
+	y_train = np.asarray(dataLabel[1])
+
+	# generate new model
+	if newModel:
+		model = trainModel(X_train,y_train)
+		saveModel(model)
+	else:
+		model = loadModel("cmodel.json","cweights.h5")
+	
 	# for i in range(100):
 	# preds = model.predict(X_train)
-	generatePredictions(X_train,model)
+	pwv = generatePredictions(model,wv,bufferSize,predictionSize)
 
-
-	writeWav(preds,rate,'testOut.wav')
+	writeWav(pwv,rate,'testOut.wav')
 
 
 	# preds *= drcmax
